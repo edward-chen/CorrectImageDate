17e5c75b4157a24c875412ccc3a9cd3e605f9825
edward.chen <jacy22@gmail.com>
2016-04-08 18:26:22 +0800

    Initial Commit

---
+++ CorrectImageDate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CorrectImageDate
+//
+//  Created by ChenYi-Hung on 2016/4/8.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CorrectImageDate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CorrectImageDate
+//
+//  Created by ChenYi-Hung on 2016/4/8.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
