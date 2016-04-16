e35db5d37835ce30bbd9ce7a286847654807da9b
edward.chen <jacy22@gmail.com>
2016-04-17 05:56:43 +0800

    [Title] CoorectImageDate for OSX
    [Description]
    1. Add NSOpenPanel for open directory
    2. Testing enumerate function for list jpg file

--- CorrectImageDate/ViewController.swift
+++ CorrectImageDate/ViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +21,52 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBOutlet weak var mCurrFolder: NSTextField!
+    
+    func printFilesInFolder() {
+        let fileManager = NSFileManager.defaultManager();
+        
+        do {
+            let contents = try fileManager.contentsOfDirectoryAtPath(mCurrFolder.stringValue);
+            
+            // TODO: using .contentsOfDirectoryAtURL to re-implement this value
+            
+            var cnt = 0;
+            let fileNames = contents as [String];
+            for fileName in fileNames {
+                if (fileName.hasSuffix(".jpg")) {
+                    print("[\(cnt)]:\(fileName)");
+                    cnt++;
+                }
+            }
+        } catch {
+            print("no file in \(mCurrFolder.stringValue)");
+            
+        }
+        
+    }
+    
+    
+    
+    @IBAction func browseFile(sender: AnyObject) {
+        let dialog = NSOpenPanel();
+        dialog.title = "Open Images Folder for adjust time"
+        dialog.canChooseDirectories = true;
+        dialog.canChooseFiles = false;
+        
+        if (dialog.runModal() == NSModalResponseOK) {
+            let result = dialog.URL;
+            
+            if (result != nil){
+                let path = result!.path!;
+                print(path);
+                
+                mCurrFolder.stringValue = path;
+                printFilesInFolder();
+            }
+        }
+    
+    }
 }
 
